b0f05ad66c452964e0f5fc00a2608eb8cfb4383d
Aniket <aaniket20@gmail.com>
2023-01-09 19:08:12 +0530

    Frontend - Integrate Add transaction API

--- frontend/src/screens/NewTransactionScreen/index.jsx
+++ frontend/src/screens/NewTransactionScreen/index.jsx
@@ -1,17 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useMemo } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import "./styles.css";
 import useAddTransaction from "./useAddTransaction";
 import useAmountState from "./useAmountState";
 
 const NewTransactionScreen = () => {
     const { customerID } = useParams();
+    const { search } = useLocation();
+    const searchParams = useMemo(() => new URLSearchParams(search), [search]);
 
     const [amount, onAmountChange] = useAmountState();
-    const [{ isSubmitting, isTransactionSuccessful, errorMessage }, onSubmit] = useAddTransaction({ customerID, amount });
+    const [{ isSubmitting, rewardPoints, errorMessage }, onSubmit] = useAddTransaction({ customerID, amount });
 
     return (
         <div className="page-container">
-            <h1>Add Transaction for {customerID}</h1>
+            <h1>Add Transaction for {searchParams.get('name')}</h1>
             <form className="add-new-transaction__page-content" onSubmit={onSubmit}>
                 <div className="add-new-transaction__input-container">
                     <label htmlFor="amount">Purchase</label>
@@ -23,14 +26,14 @@ const NewTransactionScreen = () => {
                         placeholder="Enter Amount Here"
                         onKeyDown={blockInvalidChar}
                     />
-                    {errorMessage && <div className="error-message">{errorMessage}</div>}
                 </div>
+                {errorMessage && <div className="error-message">{errorMessage}</div>}
                 <button type="submit" disabled={!amount || isSubmitting}>{isSubmitting ? 'Saving...' : 'Save Transaction'}</button>
             </form>
             {
-                (isTransactionSuccessful && !isSubmitting)
+                (rewardPoints && !isSubmitting)
                 &&
-                <h3 className="successful-transaction-message">Transaction Added Successfully ✅</h3>
+                <h3 className="successful-transaction-message">Purchase Added Successfully ✅. Earned <span>{rewardPoints}</span> reward points 💎.</h3>
             }
         </div>
     );
--- frontend/src/screens/NewTransactionScreen/useAddTransaction.js
+++ frontend/src/screens/NewTransactionScreen/useAddTransaction.js
@@ -1,28 +1,33 @@
+import axios from "axios";
 import { useCallback, useState } from "react";
-
+import { API_SERVICES } from "../../constants";
 
 const useAddTransaction = ({ customerID, amount }) => {
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
-    const [isTransactionSuccessful, setCustomerID] = useState(null);
+    const [rewardPoints, setRewardPoints] = useState(null);
 
     const onSubmit = useCallback(async (event) => {
         setErrorMessage('');
         setIsSubmitting(true);
+        setRewardPoints(null);
         event.preventDefault();
         event.stopPropagation();
         try {
-            setTimeout(() => {
-                setIsSubmitting(false);
-                setCustomerID(true);
-            }, 1000);
+            const { data } = await axios.post(API_SERVICES.ADD_TRANSACTION, {
+                amount: Number(amount),
+                customerID,
+            });
+            setRewardPoints(Number(data.rewardPoints));
         } catch (error) {
             console.error(error);
-            setErrorMessage(error.toString());
+            setErrorMessage(error?.response?.data?.message ?? error.toString());
+        } finally {
+            setIsSubmitting(false);
         }
     }, [customerID, amount]);
 
-    return [{ isSubmitting, errorMessage, isTransactionSuccessful }, onSubmit];
+    return [{ isSubmitting, errorMessage, rewardPoints }, onSubmit];
 };
 
 export default useAddTransaction;
