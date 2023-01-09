398d4372b03d35453baf0f6f0a167835fea8bcc3
Aniket <aaniket20@gmail.com>
2023-01-09 17:11:59 +0530

    Backend - Get customers API

--- backend/controllers/getCustomers.js
+++ backend/controllers/getCustomers.js
@@ -1,6 +1,15 @@
+import { database as db } from "../index.js";
 
-const getCustomers = (req, res) => {
-    res.status(200).send("Get All Customers!");
+const getCustomers = async (req, res) => {
+    // Read data from JSON file, this will set db.data content
+    await db.read();
+
+    // // If db.json doesn't exist, db.data will be null
+    // // Use the code below to set default data
+    db.data = db.data || { customers: [] };
+
+    await db.write();
+    res.status(200).send(db.data);
 };
 
 export default getCustomers;
