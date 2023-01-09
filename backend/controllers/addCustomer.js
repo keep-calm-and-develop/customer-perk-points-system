395233c997c962f5dd7c9de296c359ba92bf16f9
Aniket <aaniket20@gmail.com>
2023-01-09 17:24:08 +0530

    Backend - Add customer API

--- backend/controllers/addCustomer.js
+++ backend/controllers/addCustomer.js
@@ -1,10 +1,26 @@
+import { nanoid } from "nanoid";
+import { database as db } from "../index.js";
 
-const addCustomer = (req, res) => {
-    const { name } = req.body;
-    // customer
-    // res.status(400).send(`Customer already exists with a name ${name}`);
+const addCustomer = async (req, res) => {
+    try {
+        const { name } = req.body;
+        await db.read();
+        db.data = db.data || { customers: [] };
 
-    res.status(200).send(`Added Customer with ID ${name}`);
+        if (db.data.customers.find((customer) => customer.name === name)) {
+            res.status(400).send({ message: `Customer already exists with the name ${name}` });
+            return;
+        }
+
+        const customerID = nanoid(4);
+        db.data.customers.push({ name, customerID, transactions: [] });
+        await db.write();
+
+        res.status(200).send({ customerID });
+    } catch (error) {
+        res.status(500).send({ message: "Internal server error" });
+        console.log(error);
+    }
 };
 
 export default addCustomer;
