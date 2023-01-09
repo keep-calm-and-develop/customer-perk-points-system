418f19fb530963ae7c464779e87e27fec1333d91
Aniket <aaniket20@gmail.com>
2023-01-09 18:47:03 +0530

    Frontend - Integrate get customers API

--- frontend/src/screens/CustomersScreen/CustomerItem.jsx
+++ frontend/src/screens/CustomersScreen/CustomerItem.jsx
@@ -1,9 +1,7 @@
 import { Link } from "react-router-dom";
 
-
 const CustomerItem = ({ name, customerID }) => (
     <div className="customers-list__item">
-        <span className="customers-list__item__customerID"><span className="list__item__customerID__hash">#</span> {customerID}</span>
         <span className="customers-list__item__name">{name}</span>
         <Link className="customers-list__item__add-transaction" to={`/${customerID}/new-transaction`}>Add Transaction</Link>
     </div>
---
+++ frontend/src/screens/CustomersScreen/useGetCustomers.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_SERVICES } from "../../constants";
+
+export const useGetCustomers = () => {
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const { data } = await axios.get(`${API_SERVICES.GET_CUSTOMERS}`);
+                setCustomers(data.customers);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, []);
+
+    return {
+        customers,
+        loading,
+    };
+};
