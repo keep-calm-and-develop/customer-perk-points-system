1671ec2b94fbbabbb182205035cde2ebbdb1a78c
Aniket <aaniket20@gmail.com>
2023-02-03 19:31:08 +0530

    Backend - Add unit tests for GET /api/customers

--- backend/test/getCustomers.test.js
+++ backend/test/getCustomers.test.js
@@ -5,11 +5,25 @@ import server from "../index.js";
 chai.use(chaiHttp);
 
 describe("GET /api/customers", () => {
-    it("it should GET all the books", (done) => {
+    it("it return 1 customer", (done) => {
         chai.request(server)
             .get("/api/customers")
             .end((err, res) => {
                 expect(res.body.customers.length).to.equal(1);
+                done();
+            });
+    });
+    it("it should return customer with required fields", (done) => {
+        chai.request(server)
+            .get("/api/customers")
+            .end((err, res) => {
+                expect(res.body.customers.length).to.equal(1);
+                const customer = res.body.customers[0];
+                expect(customer).to.have.own.property("name");
+                expect(customer).to.have.own.property("customerID");
+                expect(customer).to.have.own.property("totalPoints");
+                expect(customer.monthlyPoints).to.be.an("array");
+                expect(customer.monthlyPoints.length).to.equal(3);
                 done();
             });
     });
